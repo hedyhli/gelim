104ed9c03274eb824291144b58add43a2dbb1f4c
Hedy Li <hedy@tilde.cafe>
2021-07-20 15:26:38 +0800

    fix link label printf % displays

--- gemini.go
+++ gemini.go
@@ -33,7 +33,7 @@ type GeminiResponse struct {
 var (
 	h1Style   = color.New(color.Bold).Add(color.Underline).Add(color.FgYellow).SprintFunc()
 	h2Style   = color.New(color.Bold).SprintFunc()
-	linkStyle = color.New(color.FgBlue).SprintfFunc()
+	linkStyle = color.New(color.FgBlue).SprintFunc()
 	// style only applied to first line for some reason, so removing it all together :P
 	// quoteStyle = color.New(color.Italic).SprintFunc()
 )
